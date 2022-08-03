c981e5a8c902e9a13740b1601ff9e3beda3ea527
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-08-03 20:58:13 -0300

    Operadores

---
+++ aula_10/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$i = 10;
+
+print ($i == 10 ? 'true' : 'false') . PHP_EOL;
+print ($i < 10 ? 'true' : 'false') . PHP_EOL;
+print ($i > 10 ? 'true' : 'false') . PHP_EOL;
+print ($i <= 10 ? 'true' : 'false') . PHP_EOL;
+print ($i >= 10 ? 'true' : 'false') . PHP_EOL;
+print ($i != 10 ? 'true' : 'false') . PHP_EOL;
