e180225e2231d066bc44f2c040fb838a9a488c26
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-08-28 14:18:29 -0300

    Die e sleep

---
+++ modulo_06/aula_03/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*sleep(3);
+
+echo 'Olá';
+
+sleep(2);
+
+echo 'Dois';
+
+sleep(3);
+
+echo 'TrÊs';*/
+
+echo 'Este é um código bem legal';
+
+die('<br>Morreu aqui!');
+
+echo 'Este código não será executado!';
